844fbb90205c32191bec4058020fb5a8b5b65de3
Sldvlld <sladvlad@gmail.com>
2023-05-01 14:33:35 +0300

    fixed getItems

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,25 +5,14 @@ import { Form } from './Form/Form';
 import css from './App.module.css';
 
 export function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-
+  const [contacts, setContacts] = useState(() =>
+    JSON.parse(localStorage.getItem('contacts') ?? [])
+  );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
-  useEffect(() => {
-    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-    if (savedContacts) {
-      setContacts(savedContacts);
-    }
-  }, []);
 
   const handleSubmitForm = newContact => {
     contacts.find(
